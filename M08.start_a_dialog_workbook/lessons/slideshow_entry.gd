dcb8fa02e3e81eed9de68c245bf5641470f1aba9
Mulinzhang123 <MuZhang@s.sfusd.edu>
2025-11-21 11:08:02 -0800

    lesson 4 and 5 done

---
+++ M08.start_a_dialog_workbook/lessons/slideshow_entry.gd
@@ -0,0 +1,6 @@
+class_name SlideShowEntry extends Resource
+
+@export var expression := preload("res://assets/emotion_regular.png")
+@export var character := preload("res://assets/sophia.png")
+@export_multiline var text := ""
+@export var dialogue_items: Array[SlideShowEntry] = []
